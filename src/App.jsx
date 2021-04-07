5056c8125060ecc288679dd21fe0b2a06cc4991c
hyunju <phj2309@naver.com>
2021-04-07 12:51:55 +0900

    #23 평가하기 페이지 구현 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { PureComponent }from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import React, { PureComponent } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import { observer, inject } from "mobx-react";
 
 import MainPage from "./Pages/Main";
@@ -11,30 +11,32 @@ import EnrollPage from "./Pages/Enroll";
 import EnrollDetailPage from "./Pages/EnrollDetail";
 import Chat from "./Pages/Chat";
 import ChatList from "./Pages/ChatList";
+import Evaluation from "./Pages/Evaluation";
 
 import * as Util from "@util";
 
 import "./index.scss";
 
 const App = (props) => {
-    console.log(props);
+  console.log(props);
 
-    return (
-        <React.Fragment>
-            <BrowserRouter>
-                <Route exact path="/" component={MainPage} />
-                <Route path="/main" component={MainPage} />
-                <Route path="/login" component={LoginPage} />
-                <Route path="/register" component={RegisterPage} />
-                <Route path="/pwfind" component={PwFindPage} />
-                <Route path="/infochange" component={InfoChangePage} />
-                <Route path="/enroll" component={EnrollPage} />
-                <Route path="/enrolldetail" component={EnrollDetailPage} />
-                <Route path="/chat" component={Chat} />
-                <Route path="/chatlist" component={ChatList} />
-            </BrowserRouter>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/main" component={MainPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/pwfind" component={PwFindPage} />
+        <Route path="/infochange" component={InfoChangePage} />
+        <Route path="/enroll" component={EnrollPage} />
+        <Route path="/enrolldetail" component={EnrollDetailPage} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/chatlist" component={ChatList} />
+        <Route path="/evaluation" component={Evaluation} />
+      </BrowserRouter>
+    </React.Fragment>
+  );
 };
 
 export default inject("storeMain")(observer(App));
---
+++ src/Pages/Evaluation/index.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState, useRef, memo } from "react";
+import { observer, inject } from "mobx-react";
+import { withRouter, Link } from "react-router-dom";
+
+import MainLayout from "@templates/MainLayout";
+
+import Button from "@components/Button";
+import Textarea from "@components/Textarea";
+
+import "./style.scss";
+
+import Logo from "@asset/bamboo.svg";
+import closeIcon from "@asset/close.svg";
+
+const Evaluation = () => {
+  const [content, setContent] = useState("");
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const resize = (event) => {
+    let target = event.target;
+    target.style.height = "200px";
+    target.style.height = target.scrollHeight + "px";
+  };
+
+  const uploadBtn = (e) => {};
+
+  return (
+    <MainLayout>
+      <div className="Wrapper">
+        <div className="Top">
+          <div className="Title">평가하기</div>
+          <div className="ImgWrapper">
+            <img className="closeIcon" src={closeIcon} alt="" />
+          </div>
+        </div>
+
+        <div className="ContentsWrapper">
+          <p className="Contents">쥬르리 님과의 거래를 평가해주세요!</p>
+
+          <Textarea
+            padding="5px 0px 0px 5px"
+            value={content}
+            height="200px"
+            onChange={handleContentChange}
+            onKeyUp={resize}
+            onKeyDown={resize}
+            placeholder="내용을 입력하세요."
+            margin="0px 0px 10px 0px"
+            className="content"
+          ></Textarea>
+        </div>
+
+        <div className="Bottom">
+          <Button
+            className="uploadBtn"
+            value="신고하기"
+            width="100%"
+            height="50px"
+            onClick={uploadBtn}
+          ></Button>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Evaluation;
